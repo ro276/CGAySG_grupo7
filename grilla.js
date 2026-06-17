512b822fd6314a11c8409de1e4db92ac0cfb0f35
ro276 <rosarri357@gmail.com>
2026-06-17 15:02:39 -0300

    actualizacion de la grilla, densidad de la linea mejorada
    Updated grid drawing logic to use a variable grid size and adjusted stroke weight for primary grid lines.

--- grilla.js
+++ grilla.js
@@ -1,13 +1,24 @@
+let tamañoGrilla = 100;    
+
 function drawGrid() {
   stroke(gridColor);
   // Usa el umbral de volumen que definimos en el gestor
-  strokeWeight(1); 
+  strokeWeight(3); 
   let step = 10; 
+
   
-  for (let x = 0; x < width; x += step) {
+  for (let x = 0; x < width; x += tamañoGrilla) {
     line(x, 0, x, height);
+   
   }
-  for (let y = 0; y < height; y += step) {
+  for (let y = 0; y < height; y += tamañoGrilla) {
     line(0, y, width, y);
+   
   }
+
+  strokeWeight(1);
+  for (let ychico =0; ychico<height; ychico +=step)
+      line(0,ychico,width,ychico);
+     for(let xchico = 0; xchico <width; xchico += step)
+      line(xchico,0,xchico,height);
 }
